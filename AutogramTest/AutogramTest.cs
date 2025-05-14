692277dafb298835fbb249d69f650efe37c753fa
jamesmoore <jamesmoore@whatever.com>
2025-05-14 07:58:22 +0100

    fix test

--- AutogramTest/AutogramTest.cs
+++ AutogramTest/AutogramTest.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autogram;
-
-namespace AutogramTest
+﻿namespace AutogramTest
 {
     public class AutogramTest
     {
         [Fact]
         public void Autogram_GetCounts_Test()
         {
-            var a = new Autogram.Autogram();
-            var x = a.GetActualCounts();
-            Assert.Equal(26, x.Length);
-
-            Assert.All(x, p => Assert.True(p > 0));
+            const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            var a = new Autogram.Autogram(alphabet.ToCharArray());
+            var x = a.GetActualCounts(a.ToString());
+            Assert.Equal(alphabet.Length, x.Length);
         }
     }
 }
