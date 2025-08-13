14a0861c7d327753ebf3ca4a7639442cfddcf53c
James Moore <jamesmoore@users.noreply.github.com>
2025-08-13 21:36:43 +0100

    Make CharExtensions public and simplify tests

---
+++ AutogramTest/CharExtensionsTests.cs
@@ -0,0 +1,58 @@
+using Autogram;
+
+namespace AutogramTest
+{
+    public class CharExtensionsTests
+    {
+        [Theory]
+        [InlineData(',', "comma")]
+        [InlineData('-', "hyphen")]
+        [InlineData('\'', "apostrophe")]
+        [InlineData(' ', "space")]
+        [InlineData('a', "a")]
+        public void GetCharacterName_ReturnsExpectedName(char character, string expected)
+        {
+            var result = character.GetCharacterName();
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(',', "commas")]
+        [InlineData('-', "hyphens")]
+        [InlineData('\'', "apostrophes")]
+        [InlineData(' ', "spaces")]
+        [InlineData('a', "a's")]
+        public void GetPluralisedCharacterName_ReturnsExpectedName(char character, string expected)
+        {
+            var result = character.GetPluralisedCharacterName();
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData('a', 1, "a")]
+        [InlineData('a', 2, "a's")]
+        [InlineData(' ', 1, "space")]
+        [InlineData(' ', 3, "spaces")]
+        public void GetCharacterName_WithCount_ReturnsExpected(char character, int count, string expected)
+        {
+            var result = character.GetCharacterName(count);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData('a', false)]
+        [InlineData(',', true)]
+        public void HasExtendedName_ReturnsExpected(char character, bool expected)
+        {
+            var result = character.HasExtendedName();
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GetPluralisedCharacterName_ReturnsApostropheSWithoutBackslash()
+        {
+            var result = 'a'.GetPluralisedCharacterName();
+            Assert.Equal("a's", result);
+        }
+    }
+}
