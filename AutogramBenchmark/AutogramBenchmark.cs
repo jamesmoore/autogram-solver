c7e9c0b9f8ab1f296d0473a6f30b62fd2e636512
James Moore <jamesmoore@users.noreply.github.com>
2026-02-07 18:58:17 +0000

    improve benchmark (#54)

--- AutogramBenchmark/AutogramBenchmark.cs
+++ AutogramBenchmark/AutogramBenchmark.cs
@@ -5,10 +5,12 @@ namespace AutogramBenchmark
 {
     public class AutogramBenchmark
     {
-        [Params(100)]
+        [Params(10, 50, 100)]
         public int SeedCount;
 
-        private AutogramConfig autogramConfig;
+        private AutogramConfig autogramConfig = null!;
+        private List<AutogramBytesNoStringsV4> solver4List = null!;
+        private List<AutogramBytesNoStringsV5> solver5List = null!;
 
         [GlobalSetup]
         public void Setup()
@@ -23,10 +25,61 @@ namespace AutogramBenchmark
                 "z");
         }
 
-        [Benchmark]
-        public void AutogramBytesNoStringsV4_Solve_Average_Seeds_0_100()
+        [IterationSetup(Targets = new[] { nameof(AutogramBytesNoStringsV4_Solve_Average_Batched_Seeds) })]
+        public void IterationSetupV4()
         {
-            for (int i = 0; i < 100; i++)
+            solver4List = Enumerable.Range(0, SeedCount).Select(p => new AutogramBytesNoStringsV4(autogramConfig, p)).ToList();
+        }
+
+        [IterationSetup(Targets = new[] { nameof(AutogramBytesNoStringsV5_Solve_Average_Batched_Seeds) })]
+        public void IterationSetupV5()
+        {
+            solver5List = Enumerable.Range(0, SeedCount).Select(p => new AutogramBytesNoStringsV5(autogramConfig, p)).ToList();
+        }
+
+
+        [IterationCleanup(Targets = new[] { nameof(AutogramBytesNoStringsV4_Solve_Average_Batched_Seeds) })]
+        public void CleanupIterationV4()
+        {
+            solver4List = null!;
+        }
+
+        [IterationCleanup(Targets = new[] { nameof(AutogramBytesNoStringsV5_Solve_Average_Batched_Seeds) })]
+        public void CleanupIterationV5()
+        {
+            solver5List = null!;
+        }
+
+        [Benchmark]
+        public void AutogramBytesNoStringsV4_Solve_Average_Batched_Seeds()
+        {
+            foreach (var solver in solver4List)
+            {
+                while (true)
+                {
+                    var result = solver.Iterate();
+                    if (result.Success) break;
+                }
+            }
+        }
+
+        [Benchmark]
+        public void AutogramBytesNoStringsV5_Solve_Average_Batched_Seeds()
+        {
+            foreach (var solver in solver5List)
+            {
+                while (true)
+                {
+                    var result = solver.Iterate();
+                    if (result.Success) break;
+                }
+            }
+        }
+
+        [Benchmark]
+        public void AutogramBytesNoStringsV4_Solve_For_SeedCount_With_Ctor()
+        {
+            for (int i = 0; i < SeedCount; i++)
             {
                 var x = new AutogramBytesNoStringsV4(autogramConfig, i);
                 while (true)
@@ -38,9 +91,9 @@ namespace AutogramBenchmark
         }
 
         [Benchmark]
-        public void AutogramBytesNoStringsV5_Solve_Average_Seeds_0_100()
+        public void AutogramBytesNoStringsV5_Solve_For_SeedCount_With_Ctor()
         {
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < SeedCount; i++)
             {
                 var x = new AutogramBytesNoStringsV5(autogramConfig, i);
                 while (true)
