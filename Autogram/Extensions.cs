6bb4ca484decc1f6120282921b2693c028d42ebc
James Moore <jamesmoore@users.noreply.github.com>
2026-05-23 07:13:39 +0100

    improve unordered byte span comparison performance (#89)

--- Autogram/Extensions.cs
+++ Autogram/Extensions.cs
@@ -195,6 +195,45 @@ namespace Autogram
             return true;
         }
 
+        public static bool UnorderedByteSpanEqualsWithSum(this ReadOnlySpan<byte> a, ReadOnlySpan<byte> b)
+        {
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+
+            int sumA = 0;
+            int sumB = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sumA += a[i];
+                sumB += b[i];
+            }
+
+            if (sumA != sumB)
+            {
+                return false;
+            }
+
+            Span<int> counts = stackalloc int[256];
+
+            foreach (var b1 in a)
+            {
+                counts[b1]++;
+            }
+
+            foreach (var b2 in b)
+            {
+                if (--counts[b2] < 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
         public static bool UnorderedByteSpanEquals2(this ReadOnlySpan<byte> x, ReadOnlySpan<byte> y)
         {
             if (x.Length != y.Length)
